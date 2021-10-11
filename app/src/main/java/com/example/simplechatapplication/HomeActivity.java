e63c40953c181c03d06ee2a1eb28bab849178307
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-10-11 14:46:03 +0300

    Firebase authentication is added.

--- app/src/main/java/com/example/simplechatapplication/HomeActivity.java
+++ app/src/main/java/com/example/simplechatapplication/HomeActivity.java
@@ -1,15 +1,23 @@
 package com.example.simplechatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.simplechatapplication.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     ActivityHomeBinding binding;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,29 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_profile){
+            startActivity(new Intent(HomeActivity.this,ProfileActivity.class));
+        }else if(item.getItemId() == R.id.menu_signout){
+            mAuth.signOut();
+            startActivity(new Intent(HomeActivity.this,MainActivity.class));
+        }else{
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/simplechatapplication/ProfileActivity.java
@@ -0,0 +1,25 @@
+package com.example.simplechatapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.simplechatapplication.databinding.ActivityProfileBinding;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ActivityProfileBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+
+}
