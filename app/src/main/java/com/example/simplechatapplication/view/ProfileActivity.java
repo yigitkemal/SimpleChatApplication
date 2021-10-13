a828ac40b7a7647a1a9f6acad55dc0ea4351fbf4
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-10-13 14:05:52 +0300

    Simple message application is complete. Notifications will be added.

--- app/src/main/java/com/example/simplechatapplication/view/ProfileActivity.java
+++ app/src/main/java/com/example/simplechatapplication/view/ProfileActivity.java
@@ -1,17 +1,62 @@
 package com.example.simplechatapplication.view;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.simplechatapplication.databinding.ActivityProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
 
     ActivityProfileBinding binding;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+
+    Bitmap selectedImage;
+    Uri imageData;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +64,156 @@ public class ProfileActivity extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        database = FirebaseDatabase.getInstance("https://chatapplication-c841c-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = database.getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        registerLauncher();
     }
+
+    public void getData(){
+        DatabaseReference newReference = database.getReference("Profiles");
+        newReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    HashMap<String,String> hashMap = (HashMap<String, String>) ds.getValue();
+
+                    String username = hashMap.get("useremail");
+                    if(username.equals(mAuth.getCurrentUser().getEmail())){
+                        String userAge = hashMap.get("userage");
+                        String userImage = hashMap.get("userimageurl");
+
+                        if(userAge != null && userImage != null)
+                        Picasso.get().load(userImage).into(binding.imageviewProfile, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                Toast.makeText(getApplicationContext(),"Fotoğraf geldi..",Toast.LENGTH_LONG).show();
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                Toast.makeText(getApplicationContext(),"Fotoğraf gelemedi..",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void upload(View view) {
+        final UUID uuid = UUID.randomUUID();
+        String imageName = "images/"+uuid+".jpg";
+        StorageReference newReference = storageReference.child(imageName);
+
+        newReference.putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                StorageReference profileImageReference = FirebaseStorage.getInstance().getReference("images/"+uuid+".jpg");
+
+                profileImageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String downloadUrl = uri.toString();
+                        String userAge = binding.edittextAge.getText().toString();
+                        String userEmail = mAuth.getCurrentUser().getEmail().toString();
+
+                        UUID uuid1 = UUID.randomUUID();
+                        databaseReference.child("Profiles").child(uuid1.toString()).child("userimageurl").setValue(downloadUrl);
+                        databaseReference.child("Profiles").child(uuid1.toString()).child("userage").setValue(userAge);
+                        databaseReference.child("Profiles").child(uuid1.toString()).child("useremail").setValue(userEmail);
+
+                        Toast.makeText(getApplicationContext(),"Kayıt işlemi başarılı...",Toast.LENGTH_LONG).show();
+
+                        startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void selectPicture(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission Needed for Gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // reequest permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }else{
+                // request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }else{
+            //going to gallery
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+
+    }
+
+
+    private void registerLauncher(){
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+                    if(intentFromResult != null){
+                        imageData = intentFromResult.getData();
+
+                        try{
+                            if(Build.VERSION.SDK_INT >= 28){
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(),imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageviewProfile.setImageBitmap(selectedImage);
+                            }else{
+                                selectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(),imageData);
+                                binding.imageviewProfile.setImageBitmap(selectedImage);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    // permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }else{
+                    //permission denied
+                    Toast.makeText(ProfileActivity.this, "Permission Needed!", Toast.LENGTH_LONG).show();;
+                }
+            }
+        });
+    }
+
 
 
 }
